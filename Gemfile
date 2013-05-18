ce3416a238c7b3cb69d9e09bbe3daac2b1b14dcd
Nate <nathan@arritt.com>
2013-05-18 08:55:32 -0700

    Automated tests with Guard

--- Gemfile
+++ Gemfile
@@ -14,9 +14,9 @@ gem 'jquery-rails', '2.0.2'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
-  # gem 'guard-rspec', '1.2.1'
-  # gem 'guard-spork', '1.2.0'  
-  # gem 'spork', '0.9.2'
+  gem 'guard-rspec', '1.2.1'
+  gem 'guard-spork', '1.2.0'  
+  gem 'spork', '0.9.2'
 end
 
 # Gems used only for assets and not required
@@ -33,6 +33,8 @@ end
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'rb-inotify', '~> 0.9'
+  gem 'libnotify', '0.5.9'
   gem 'factory_girl_rails', '4.1.0'
   gem 'cucumber-rails', '1.2.1', :require => false
   gem 'database_cleaner', '0.7.0'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,19 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "Should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', text: 'Sample App')
     end
 
     it "Should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-        text: "Ruby on Rails Tutorial Sample App | Home")
+        text: "#{base_title} | Home")
     end
 
   end
@@ -21,13 +23,13 @@ describe "Static pages" do
 
     it "Should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', text: 'Help')
     end
 
     it "Should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-        text: "Ruby on Rails Tutorial Sample App | Help")
+        text: "#{base_title} | Help")
     end
 
   end
@@ -36,13 +38,28 @@ describe "Static pages" do
 
     it "Should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', text: 'About Us')
     end
 
     it "Should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-        text: "Ruby on Rails Tutorial Sample App | About Us")
+        text: "#{base_title} | About Us")
+    end
+
+  end
+
+  describe "Contact page" do
+
+    it "Should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "Should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+        text: "#{base_title} | Contact")
     end
 
   end
